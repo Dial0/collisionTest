d709e9083ed34199cda3b163b8ee6071b32c199e
Ethan <ethansimpson@xtra.co.nz>
2023-08-20 11:36:31 +1200

    edge collision

--- main.c
+++ main.c
@@ -30,17 +30,25 @@
 //------------------------------------------------------------------------------------
 
 typedef enum {
-    NONE = 0, SURFACE, EDGE, POINT, INVALID;
+    NONE = 0, SURFACE, EDGE, POINT, INVALID
 } colType;
 
 typedef struct {
     colType type;
     Vector3 tri[3];
     Vector3 closestPoint;
-    Vector3 backstepStart;
-    Vector3 BackstepEnd;
-
+    Vector3 shellPoint;
     Vector3 newSpherePos;
+
+    //Edge
+    Vector3 ep1;
+    Vector3 ep2;
+    Vector3 colPlaneTangent;
+    Vector3 colPlaneShell;
+    Vector3 planePoints[4];
+    Vector3 colPlaneIntPoint;
+    Vector3 newEdgePoint;
+    Vector3 newShellPoint;
 
 } triColEvent;
 
@@ -50,6 +58,19 @@ void DrawArrow(Vector3 start, Vector3 end, float size, Color color) {
     Vector3 arrowStart = Vector3Add(start,Vector3Scale(dir, length*0.8f));
     DrawCylinderEx(arrowStart, end,size*2,0.00f,10,color);
     DrawCylinderEx(start, arrowStart,size,size,10,color);
+}
+
+bool rayPlaneIntersect(Vector3 rayOrigin, Vector3 rayDir, Vector3 planeOrigin, Vector3 planeNormal, float* t) {
+    *t = 0.0f;
+    float denom = Vector3DotProduct(planeNormal, rayDir);
+    if (denom == 0) {
+        return false;
+    }
+    float d = -Vector3DotProduct(planeNormal, planeOrigin);
+    float numer = Vector3DotProduct(planeNormal, rayOrigin) + d;
+
+    *t = -(numer / denom);
+    return true;
 }
 
 Vector3 getTriangleNormal(Vector3 v1, Vector3 v2, Vector3 v3) {
@@ -84,7 +105,7 @@ Vector3 nearestPointOnLine(Vector3 l1, Vector3 l2, Vector3 p, bool* edge) {
 
 typedef struct {
     colType collisionType;
-    Vector3 closestPoint
+    Vector3 pos;
     Vector3 edgePoint1;
     Vector3 edgePoint2;
 }closestPointOnTri;
@@ -98,10 +119,18 @@ closestPointOnTri closestPointTriangle(Vector3 origin, Vector3 p1, Vector3 p2, V
     Vector3 planeNormal = getTriangleNormal(p1, p2, p3);
     Vector3 inverseNormal = Vector3Subtract((struct Vector3) { 0.0f, 0.0f, 0.0f }, planeNormal);
 
-    Vector3 OriginintersectionPoint;
-    Ray OriginintersectionRay = { origin ,Vector3Normalize(inverseNormal) };
-    bool Originhit = rayPlaneCollision(OriginintersectionRay, p1, p2, p3, &OriginintersectionPoint);
+    float intersectTime = 0.0f;
+    rayPlaneIntersect(origin, inverseNormal, p1, planeNormal, &intersectTime);
+    Vector3 OriginintersectionPoint = { origin.x + intersectTime * inverseNormal.x,
+                                    origin.y + intersectTime * inverseNormal.y,
+                                    origin.z + intersectTime * inverseNormal.z };
+
     float OrigindistToPlane = Vector3Distance(origin, OriginintersectionPoint);
+
+    //Vector3 OriginintersectionPoint;
+    //Ray OriginintersectionRay = { origin ,Vector3Normalize(inverseNormal) };
+    //bool Originhit = rayPlaneCollision(OriginintersectionRay, p1, p2, p3, &OriginintersectionPoint);
+    //float OrigindistToPlane = Vector3Distance(origin, OriginintersectionPoint);
 
     Vector3 closestPoint;
     Vector3 baryPoint = Vector3Barycenter(OriginintersectionPoint, p1, p2, p3);
@@ -147,24 +176,91 @@ closestPointOnTri closestPointTriangle(Vector3 origin, Vector3 p1, Vector3 p2, V
         }
     }
 
-    result.closestPoint = closestPoint;
+    result.pos = closestPoint;
+
+    return result;
 }
 
 
 
-triColEvent sphereTriCol(Vector3 Tri[3], Vector3 spherePos, Vector3 sphereDir, float moveDist) {
+triColEvent sphereTriCol(Vector3* Tri, Vector3 spherePos, Vector3 sphereDir, float moveDist) {
 
     triColEvent result;
 
-    closestPointOnTri closestPoint = closestPointTriangle(spherePos,Tri[0],Tri[1],Tri[2]);
+    Vector3 p1 = Tri[0];
+    Vector3 p2 = Tri[1];
+    Vector3 p3 = Tri[2];
 
-    if(Vector3Distance(spherePos,closestPoint) > 1.0f) {
+    closestPointOnTri closestPoint; 
+    closestPoint = closestPointTriangle(spherePos, p1, p2, p3);
+
+    result.closestPoint = closestPoint.pos;
+
+    if(Vector3Distance(spherePos,closestPoint.pos) > 1.0f) {
         result.type = NONE;
         return result;
     }
 
-    if(closestPointOnTri.type == POINT) {
-        
+    if(closestPoint.collisionType == POINT) {
+        Ray pointShell = { closestPoint.pos, sphereDir };
+        RayCollision shellIntersection = GetRayCollisionSphere(pointShell, spherePos, 1.0f);
+        result.shellPoint = shellIntersection.point;
+        Vector3 negativeSphereDir = Vector3Subtract((struct Vector3) { 0.0f, 0.0f, 0.0f }, sphereDir);
+        Vector3 backstep = Vector3Scale(negativeSphereDir, shellIntersection.distance);
+        result.newSpherePos = Vector3Add(spherePos, backstep);
+        result.type = POINT;
+        return result;
+    }
+    if (closestPoint.collisionType == EDGE) {
+        result.type = EDGE;
+        result.ep1 = closestPoint.edgePoint1;
+        result.ep2 = closestPoint.edgePoint2;
+        Vector3 edgeNormal = Vector3Normalize(Vector3Subtract(closestPoint.edgePoint2, closestPoint.edgePoint1));
+        Vector3 colPlaneNormal = Vector3Normalize(Vector3Subtract(spherePos, closestPoint.pos));
+        Vector3 edgeTangent = Vector3CrossProduct(edgeNormal, colPlaneNormal);
+
+        { //Build points to display the plane, not needed in actual code, only debug
+            Vector3 negativeEdgeNormal = Vector3Subtract((struct Vector3) { 0.0f, 0.0f, 0.0f }, edgeNormal);
+            Vector3 negativeEdgeTangent = Vector3Subtract((struct Vector3) { 0.0f, 0.0f, 0.0f }, edgeTangent);
+            result.planePoints[0] = Vector3Add(Vector3Add(closestPoint.pos, edgeNormal), negativeEdgeTangent);
+            result.planePoints[1] = Vector3Add(Vector3Add(closestPoint.pos, negativeEdgeNormal), negativeEdgeTangent);
+            result.planePoints[2] = Vector3Add(Vector3Add(closestPoint.pos, negativeEdgeNormal), edgeTangent);
+            result.planePoints[3] = Vector3Add(Vector3Add(closestPoint.pos, edgeNormal), edgeTangent);
+        }
+        Ray pointShell = { closestPoint.pos, sphereDir };
+        RayCollision shellIntersection = GetRayCollisionSphere(pointShell, spherePos, 1.0f);
+        result.shellPoint = shellIntersection.point;
+        Vector3 negativeSphereDir = Vector3Subtract((struct Vector3) { 0.0f, 0.0f, 0.0f }, sphereDir);
+        Vector3 backstep = Vector3Scale(negativeSphereDir, shellIntersection.distance);
+        result.newSpherePos = Vector3Add(spherePos, backstep);
+
+        result.colPlaneTangent = edgeTangent;
+
+        Vector3 negativeColPlaneNormal = Vector3Subtract((struct Vector3) { 0.0f, 0.0f, 0.0f }, colPlaneNormal);
+        Ray colPlaneRay = { closestPoint.pos, negativeColPlaneNormal };
+        RayCollision shellcolPlane = GetRayCollisionSphere(colPlaneRay, spherePos, 1.0f);
+        result.colPlaneShell = shellcolPlane.point;
+
+        float colPlaneIntTime = 0.0f;
+        rayPlaneIntersect(shellcolPlane.point, negativeSphereDir, closestPoint.pos, negativeColPlaneNormal, &colPlaneIntTime);
+
+        Vector3 colPlaneIntersectionPoint = { shellcolPlane.point.x + colPlaneIntTime * negativeSphereDir.x,
+                                shellcolPlane.point.y + colPlaneIntTime * negativeSphereDir.y,
+                                shellcolPlane.point.z + colPlaneIntTime * negativeSphereDir.z };
+
+        result.colPlaneIntPoint = colPlaneIntersectionPoint;
+
+        bool onEdge;
+        Vector3 ep = nearestPointOnLine(closestPoint.edgePoint1, closestPoint.edgePoint2, colPlaneIntersectionPoint, &onEdge);
+
+        Ray newPointShell = { ep, sphereDir };
+        RayCollision newShellIntersection = GetRayCollisionSphere(newPointShell, spherePos, 1.0f);
+
+        result.newEdgePoint = ep;
+        result.newShellPoint = newShellIntersection.point;
+
+
+        return result;
     }
 
     result.type = INVALID;
@@ -176,15 +272,25 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
 
     Color mainSphereColor = Fade(BLUE, 0.3f);
+    Color newSphereColor = Fade(PURPLE, 0.3f);
 
-    Vector3 ColTri[3] = { { -5.0f, 0.0f, 1.0f },{ -0.5f, 0.0f, 1.0f },{ 0.0f, 5.0f, 0.0f } };
+    //POINT
+    //Vector3 ColTri[3] = { { -0.091649f, -0.023814f, 0.851113f },{ 0.497118f, 0.976186f, 1.65942f },{ 0.497118f, -0.023814f, 1.65942f } };
+
+
+    //EDGE
+    //Vector3 ColTri[3] = { { -0.091649f, 1.29442f, 0.310159f },{ 0.497118f, 0.976186f, 1.65942f },{ 0.497118f, -0.579771f, 0.897624f } };
+
+    //EDGE HARD
+    Vector3 ColTri[3] = { { -0.681478f, -0.158351f, 0.287404f },{ 0.254617f, 0.976186f, 2.51794f },{ 0.859759f, 0.335812f, 1.83313f } };
+
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -196,7 +302,8 @@ int main(void)
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
-
+        triColEvent Col = sphereTriCol(ColTri, (struct Vector3) { 0.0f, 0.0f, 0.0f }, (struct Vector3) { 0.0f, 0.0f, 1.0f }, 1.0f);
+        
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -206,12 +313,66 @@ int main(void)
 
             ClearBackground(RAYWHITE);
             BeginMode3D(camera);
-                DrawGrid(10,1.0f);
-                //rlDisableDepthTest();
-                DrawArrow((struct Vector3){0.0f,0.0f,0.0f}, (struct Vector3){1.0f,0.0f,0.0f},0.05f,GREEN);
-                DrawTriangle3D(ColTri[0],ColTri[1],ColTri[2],BLUE);
-                DrawSphere( (struct Vector3){0.0f,0.0f,0.0f}, 0.1f, RED);
-                DrawSphere( (struct Vector3){0.0f,0.0f,0.0f}, 1.0f, mainSphereColor);
+
+                DrawGrid(10, 1.0f);
+
+                if (Col.type == POINT) {
+                    rlEnableDepthTest();
+                    DrawArrow((struct Vector3) { 0.0f, 0.0f, 0.0f }, (struct Vector3) { 0.0f, 0.0f, 1.0f }, 0.01f, GREEN);
+                    DrawArrow(Col.closestPoint, Col.shellPoint, 0.01f, PURPLE);
+                    DrawArrow((struct Vector3) { 0.0f, 0.0f, 0.0f }, Col.newSpherePos, 0.01f, PURPLE);
+                    DrawTriangle3D(ColTri[0], ColTri[1], ColTri[2], GRAY);
+                    DrawSphere((struct Vector3) { 0.0f, 0.0f, 0.0f }, 0.01f, RED);
+                    DrawSphere(Col.newSpherePos, 0.01f, RED);
+                    DrawSphere(Col.closestPoint, 0.01f, ORANGE);
+                    DrawSphere(Col.shellPoint, 0.01f, PURPLE);
+
+                    rlDrawRenderBatchActive();
+                    rlDisableDepthTest();
+                    DrawSphere(Col.newSpherePos, 1.0f, newSphereColor);
+                    DrawSphere((struct Vector3) { 0.0f, 0.0f, 0.0f }, 1.0f, mainSphereColor);
+                }
+                if (Col.type == EDGE) {
+
+                    rlEnableDepthTest();
+
+                    //sphere movement Vector
+                    DrawArrow((struct Vector3) { 0.0f, 0.0f, 0.0f }, (struct Vector3) { 0.0f, 0.0f, 1.0f }, 0.01f, GREEN);
+                    //collision plane
+                    DrawTriangle3D(ColTri[0], ColTri[1], ColTri[2], GRAY);
+                    //sphere origin
+                    DrawSphere((struct Vector3) { 0.0f, 0.0f, 0.0f }, 0.01f, RED);
+                    //initial closest point
+                    DrawSphere(Col.closestPoint, 0.02f, ORANGE);
+
+                    //vector through closest point to shell of sphere, also shows the collision plane normal
+                    DrawArrow((struct Vector3) { 0.0f, 0.0f, 0.0f }, Col.colPlaneShell, 0.01f, ORANGE);
+                    //edge normal
+                    DrawArrow(Col.ep1, Col.ep2, 0.01f, PURPLE);
+                    //plane tangent
+                    DrawArrow(Col.closestPoint, Vector3Add(Col.closestPoint,Col.colPlaneTangent), 0.01f, RED);
+
+                    //collision plane intersection
+                    DrawArrow(Col.colPlaneShell, Col.colPlaneIntPoint, 0.01f, ORANGE);
+                    DrawSphere(Col.colPlaneIntPoint, 0.01f, GREEN);
+
+                    //new tri closest point to shell
+                    DrawArrow(Col.newEdgePoint, Col.newShellPoint, 0.01f, PURPLE);
+                    DrawSphere(Col.newEdgePoint, 0.01f, PINK);
+                    
+                    
+                    
+
+                    rlDrawRenderBatchActive();
+                    rlDisableDepthTest();
+
+                    DrawTriangle3D(Col.planePoints[0], Col.planePoints[1], Col.planePoints[2], Fade(YELLOW, 0.3f));
+                    DrawTriangle3D(Col.planePoints[2], Col.planePoints[3], Col.planePoints[0], Fade(YELLOW, 0.3f));
+                    DrawSphere(Col.newSpherePos, 1.0f, newSphereColor);
+                    DrawSphere((struct Vector3) { 0.0f, 0.0f, 0.0f }, 1.0f, mainSphereColor);
+                }
+
+            
             //DrawTriangle3D(v1, v2, v3, RED);
             EndMode3D();
 
